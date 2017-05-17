a010c93fe4e4a31511ad5e8ed1940da5bae9006e
craig-mulligan <craig@resin.io>
2017-05-17 13:08:10 +0100

    subscriptions done

--- src/index.js
+++ src/index.js
@@ -1,69 +1,33 @@
-'use strict'
-const Influx = require('influx');
+'use strict';
 const debug = require('debug-levels')('agile-data');
-
 const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
-const _ = require('lodash')
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const dbName = process.env.AGILE_DATA_DB_NAME || 'agile_db';
 const dbMeasurement = process.env.AGILE_DATA_DB_MEASUREMENT || 'sensors';
+const subscriptionRoutes = require('./routes/subscription');
 
-const subscriptions = require('./subscriptions');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/ping', function (req, res) {
   res.send('OK')
 })
 
-app.route('/api/subscription')
-  .get((req, res, next) => {
-    subscriptions.get()
-    .then((data) => res.send(data))
-    .catch(next)
-  })
+app.use('/api/subscription', subscriptionRoutes)
+app.use('/api/record', recordRoutes)
 
-app.route('/api/subscription/:deviceID/:componentID')
-  .get((req, res, next) => {
-    subscriptions.get(req.params)
-    .then((data) => res.send(data))
-    .catch(next)
-  })
-  .post((req, res, next) => {
-    const newSub = _.assign({
-      interval: req.body.interval
-    }, req.params)
-
-    subscriptions.create(newSub)
-    .then((data) => res.send(data))
-    .catch(next)
-  })
-  .put((req, res, next) => {
-    const newSub = _.assign({
-      interval: req.body.interval
-    }, req.params)
-
-    subscriptions.update(newSub)
-    .then((data) => res.send(data))
-    .catch(next)
-  })
-  .delete((req, res, next) => {
-    subscriptions.delete(req.params)
-    .then(() => res.send())
-    .catch(next)
-  })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-app.use(function(err, req, res) {
+app.use((err, req, res) => {
   switch (err.name) {
     case 'subscription already exists':
       return res.status(400).send(err);
     default:
       return res.status(500).send(err);
   }
-});
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
