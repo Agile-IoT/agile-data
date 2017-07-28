143634e80988e7656af6a76cc32d55dc3150a820
Philippe Krief <philippe.krief@eclipse.org>
2017-07-28 11:01:07 +0200

    Fixing comments in JavaScript files !

--- src/routes/record.js
+++ src/routes/record.js
@@ -1,13 +1,13 @@
-#-------------------------------------------------------------------------------
-# Copyright (C) 2017 Resin.io.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
-# 
-# Contributors:
-#     Resin.io - initial API and implementation
-#-------------------------------------------------------------------------------
+/*******************************************************************************
+ *Copyright (C) 2017 Resin.io.
+ *All rights reserved. This program and the accompanying materials
+ *are made available under the terms of the Eclipse Public License v1.0
+ *which accompanies this distribution, and is available at
+ *http://www.eclipse.org/legal/epl-v10.html
+ *
+ *Contributors:
+ *    Resin.io - initial API and implementation
+ ******************************************************************************/
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
